98dbcf5922e3fb81824964b9a621342fce930551
Sigrid <sigrid.huemer@posteo.at>
2020-08-30 13:57:23 +0200

    add node version to package.json

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   require('dotenv').config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
